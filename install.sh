67964487d636861f8ae04b96ed01325c2782c341
852us <paul.sunspipe@gmail.com>
2023-03-14 20:52:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 VERSION="0.0.1"
 
 OS=$(grep "^NAME=" /etc/os-release | cut -d "=" -f 2)
-OS_VERSION=$(grep "^VERSION=" /etc/os-release | cut -d "=" -f 2)
+OS_VERSION=$(grep "^VERSION_ID=" /etc/os-release | cut -d "=" -f 2)
 echo $OS
 echo $OS_VERSION
