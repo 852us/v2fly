a4d7b615b996ebfe613b8eab83d6672a7ebeb609
852us <paul.sunspipe@gmail.com>
2023-03-15 14:54:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ VERSION="0.0.1"
 
 pkg_cmd() {
   OS_TYPE=$(awk -F'"' '/^ID_LIKE=/{print $2}' /etc/os-release)
-  echo $OS_TYPE
   case $OS_TYPE in
   "debian"):
     echo Debian-like Linux, including Debian and Ubuntu Linux.
+    PKG_CMD="apt"
     ;;
   "fedora"):
     echo Fedora-like Linux, including Red Hat, Centos, and Fedora Linux.
+    PKG_CMD="yum"
     ;;
   esac
+  echo Package Manament Tools: $PKG_CMD
 }
 
 pkg_cmd
