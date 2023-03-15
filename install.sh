dfda0e4af618a8ab158c3d7709cf7e7ef84d9659
852us <paul.sunspipe@gmail.com>
2023-03-15 14:51:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VERSION="0.0.1"
 
 pkg_cmd() {
   OS_TYPE=$(awk -F'=' '/^ID_LIKE=/{print $2}' /etc/os-release)
-  echo $OS_TYPE
+  echo $OS_TYPE $OS_TYPE
   case $OS_TYPE in
   "debian\n"):
     echo Debian-like Linux, including Debian and Ubuntu Linux.
