13d19ac20bbc1aca2181afddbfa7cbde6a9ab75f
852us <paul.sunspipe@gmail.com>
2023-03-21 15:16:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ download_caddy() {
 	caddy_download_link="https://github.com/caddyserver/caddy/releases/download/"
 	caddy_download_link="${caddy_download_link}${caddy_latest_ver}/caddy_${caddy_latest_ver_num}_linux_${caddy_arch}.tar.gz"
 
-  caddy_current_ver=$(caddy version) | grep ${caddy_latest_ver_num}
+  caddy_current_ver=$(caddy version | grep ${caddy_latest_ver_num})
   if [[ ${caddy_current_ver} = ${caddy_latest_ver_num} ]]; then
     echo -e "${RED}Caddy当前安装版本：{caddy_current_ver}，与最新版本：${caddy_latest_ver_num}相同 ..."
     exit 1
